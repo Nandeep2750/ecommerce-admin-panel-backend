0c9807a3ae6379766e5ac1767808fb6d4d59e94c
nandeep21 <nandeep.barochiya@openxcell.com>
2023-04-14 11:54:46 +0530

    Refresh Token API

--- controllers/cms/authController.js
+++ controllers/cms/authController.js
@@ -179,6 +179,118 @@ class AuthController extends CmsController {
         }
     }
 
+    /** ---------- Refresh Token ----------
+    * 
+    * @param {String} token - token.
+    * @param {String} refreshToken - refreshToken.
+    * 
+    * @return {Object} - It will give us new token & new refreshToken.
+    * 
+    * ---------------------------------------- */
+
+    refreshToken = (req, res, next) => {
+
+        try {
+            const { body } = req;
+
+            const validationSchema = Joi.object({
+                token: Joi.string().required(),
+                refreshToken: Joi.string().required()
+            });
+            const { error, value } = validationSchema.validate(body);
+
+            if (error) {
+                return res.status(STATUS.BAD_REQUEST_CODE).json({
+                    message: error.message,
+                });
+            } else {
+                let decoded = jwt.decode(value.token);
+
+                if (decoded) {
+
+                    AdminModel.findById(decoded.adminId)
+                        .select(["refreshToken", "refreshTokenCreatedAt"])
+                        .exec()
+                        .then((admin) => {
+
+                            if (admin) {
+                                let current_datetime = moment()
+                                let refresh_token_createdAt = moment(admin.refreshTokenCreatedAt)
+                                let day_difference = current_datetime.diff(refresh_token_createdAt, 'days');
+
+                                if (admin.refreshToken !== value.refreshToken) {
+                                    return res.status(STATUS.NON_AUTHORITATIVE_INFORMATION_CODE).json({
+                                        message: "Refresh token is not match with user data, Please check refresh token."
+                                    })
+                                } else if (day_difference >= ADMIN_CONFIG.REFRESH_TOKEN_VALID_FOR_DAYS) {
+                                    return res.status(STATUS.NON_AUTHORITATIVE_INFORMATION_CODE).json({
+                                        message: "Refresh token expired."
+                                    })
+                                } else {
+                                    AdminModel.findByIdAndUpdate(admin._id, {
+                                        refreshToken: uuidv4(),
+                                        refreshTokenCreatedAt: moment().valueOf()
+                                    }, { new: true }).exec().then((admin) => {
+                                        if (admin) {
+
+                                            let token = jwt.sign({
+                                                adminId: admin._id
+                                            }, ADMIN_CONFIG.JWT_KEY, {
+                                                expiresIn: ADMIN_CONFIG.TOKEN_EXPIRES_IN,
+                                            })
+
+                                            admin = JSON.parse(JSON.stringify(admin));
+                                            admin.token = token
+
+                                            let response_data = {
+                                                token: token,
+                                                refreshToken: admin.refreshToken
+                                            }
+
+                                            return res.status(STATUS.SUCCESS_CODE).json({
+                                                message: "New token generated successfully.",
+                                                data: response_data
+                                            })
+                                        } else {
+                                            return res.status(STATUS.NOT_FOUND_CODE).json({
+                                                message: "No admin available for given admin id."
+                                            })
+                                        }
+
+                                    }).catch((err) => {
+                                        console.error("🚀 ~ file: authController.js:261 ~ AuthController ~ .then ~ err:", err)
+                                        return res.status(STATUS.INTERNAL_SERVER_ERROR_CODE).json({
+                                            message: err.message
+                                        })
+                                    })
+                                }
+                            } else {
+                                return res.status(STATUS.NOT_FOUND_CODE).json({
+                                    message: "No admin available for given id please check token."
+                                })
+                            }
+                        }).catch((err) => {
+                            console.error("🚀 ~ file: authController.js:274 ~ AuthController ~ .then ~ err:", err)
+                            return res.status(STATUS.INTERNAL_SERVER_ERROR_CODE).json({
+                                message: err.message
+                            })
+                        })
+
+                } else {
+                    return res.status(STATUS.BAD_REQUEST_CODE).json({
+                        message: "Current token is invalid.",
+                    });
+                }
+            }
+
+        } catch (err) {
+            console.error("🚀 ~ file: authController.js ~ line 289 ~ AuthController ~ err", err)
+            return res.status(STATUS.INTERNAL_SERVER_ERROR_CODE).json({
+                message: "Something went wrong.",
+            });
+        }
+    }
+
 }
 
 module.exports = new AuthController();
--- routes/cms/auth.js
+++ routes/cms/auth.js
@@ -4,5 +4,6 @@ const authController = require('../../controllers/cms/authController');
 
 router.post('/register',authController.registerAdmin);
 router.post('/login',authController.loginAdmin);
+router.post('/refresh-token',authController.refreshToken);
 
 module.exports = router;
